10811694eede5ce68525c96f4118cffddf45b3fb
caddelacruz <152310179+caddelacruz@users.noreply.github.com>
2024-09-22 15:17:29 -0600

    Edit Root Page
    edit my root page to show the web page look of my week-2 assignment

--- app/page.js
+++ app/page.js
@@ -1,7 +1,16 @@
+import Link from "next/link";
+
 export default function Page() {
   return (
     <main>
-      <h1>Shopping List</h1>
+      <header>
+        <h1>CPRG 306: Web Development 2 - Assignments</h1>  
+        <ul>  
+          <p>
+            <Link href="/week-2">Week-2 Assignment</Link> 
+          </p>
+        </ul>
+      </header>
     </main>
   );
 }
