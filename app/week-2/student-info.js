4523c32b61009986650b1f6ecccc68bbd4731833
caddelacruz <152310179+caddelacruz@users.noreply.github.com>
2024-09-22 14:32:52 -0600

    Created student-info component
    linked component to my github repository

---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+    return (
+        <header>
+            <h1>Catherine Dela Cruz</h1>
+            <p>
+                <Link href = "https://github.com/caddelacruz">https://github.com </Link>
+            </p>
+        </header>
+    )
+}
